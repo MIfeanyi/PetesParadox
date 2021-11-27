95fb1d1f917e563b9a132015773b4b3e024abe78
Michael Redford <michaeliredford@gmail.com>
2021-11-27 18:29:26 +0900

    bullet now kills enemy

--- Project/assets/common/projectiles/bullet.gd
+++ Project/assets/common/projectiles/bullet.gd
@@ -8,7 +8,6 @@ var slope_x
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.playing=true
-	set_contact_monitor(true)
 	pass # Replace with function body.
 
 func shoot(flip,north):
@@ -21,7 +20,8 @@ func shoot(flip,north):
 	
 func _handle_collision(delta):
 	for i in get_colliding_bodies():
-		queue_free()
+		print(i)
+		#queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_handle_collision(delta)
@@ -36,4 +36,11 @@ func _on_Timer_timeout():
 
 func _on_bullet_body_entered(body):
 	queue_free()
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("enemy"):
+		body.queue_free()
+		queue_free()
 	pass # Replace with function body.
