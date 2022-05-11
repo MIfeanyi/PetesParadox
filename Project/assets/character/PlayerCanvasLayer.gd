61281c7448678b5d185460ec8eea8c040eb40e9f
Michael Redford <michaeliredford@gmail.com>
2022-05-11 15:20:39 +0900

    new sprites

--- Project/assets/character/PlayerCanvasLayer.gd
+++ Project/assets/character/PlayerCanvasLayer.gd
@@ -1,12 +1,20 @@
 extends CanvasLayer
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Gobal.connect("life_changed",self,"update_gui")
 
 	pass # Replace with function body.
+
+func set_ui_amount(ui,v):
+	for i in ui.get_children():
+		print(i.name)
+		if(int(i.name))<=v:
+			i.visible=true
+		else:
+			i.visible=false
+			pass
+		
 
 func update_gui():
 	for i in $MarginContainer/UI/Lifes.get_children():
